7ba3b136b036be0c5485f015fbb7c002f56c8c4a
PraneethKulukuri26 <praneethkulukuri@gmail.com>
2024-11-06 10:59:14 +0530

    import java.io.IOException;

--- src/main/java/Controller/HomePage.java
+++ src/main/java/Controller/HomePage.java
@@ -1,16 +1,15 @@
-package Servlets;
+package Controller;
 
 import java.io.IOException;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-public class HomeServlet extends HttpServlet{
+public class HomePage extends HttpServlet{
 	
 	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 		
